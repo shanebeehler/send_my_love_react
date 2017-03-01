552fb78cd73a4058996a5fc9ff5de70980d82c3c
Shane <shanebeehler@me.com>
2017-03-01 16:44:24 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,30 +1,13 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080
+const express = require('express')
+const path = require('path');
+const port = process.env.PORT || 8080
+const app = express();
 
-// using webpack-dev-server and middleware in development environment
-if(process.env.NODE_ENV !== 'production') {
-  var webpackDevMiddleware = require('webpack-dev-middleware');
-  var webpackHotMiddleware = require('webpack-hot-middleware');
-  var webpack = require('webpack');
-  var config = require('./webpack.config');
-  var compiler = webpack(config);
+app.use(express.static(__dirname, 'build'));
 
-  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-  app.use(webpackHotMiddleware(compiler));
-}
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/build/index.html')
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 
-app.listen(PORT, function(error) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-  }
-});
+app.listen(port);
+console.log('Server started');
