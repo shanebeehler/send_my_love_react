5291f818af275f85f047a1c98e1363cc82414dbf
Shane <shanebeehler@me.com>
2017-03-01 16:50:15 -0500

    change server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080
 const app = express();
 
-app.use(express.static(__dirname, 'build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './build/index.html'));
