b1b6c1a2f570413c70768ddddcf49a7365819d24
Shane <shanebeehler@me.com>
2017-03-02 22:38:00 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080
-const fb_app_id = process.env.FB_APP_ID
-console.log(fb_app_id);
+var fb_app_id = process.env.FB_APP_ID
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -11,5 +10,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 
+
 app.listen(port);
-console.log('Server started');
+console.log(fb_app_id);
