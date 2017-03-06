cd4a293cc33f5a6ffdce4dcf9abd0abf4aa4f4af
Shane <shanebeehler@me.com>
2017-03-06 17:06:43 -0500

    remove unused import

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,6 @@
 import React          from 'react';
 import FacebookLogin  from '../containers/facebook_login';
 import LocationFinder from '../containers/location_finder';
-import FormContainer  from '../containers/form_container';
 import Stats          from '../containers/stats';
 
 const Home = (props) => {
