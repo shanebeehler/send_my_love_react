08a086b045587ab40bd7d9498f101e2b7c2d6f00
Shane <shanebeehler@me.com>
2017-03-08 15:21:16 -0500

    fix dev bug

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React      from 'react';
+import ReactSwipe from 'react-swipe';
 
 const Welcome = (props) => {
 
   return (
     <div className="welcome">
       <div className="copy-background">
-        <div className="copy">
-          <p>
-            Share your name and location
-            to join in on the love chain.
-
-            A simple daily connection.
-          </p>
-        </div>
+        <ReactSwipe className="carousel" swipeOptions={{continuous: false}}>
+          <div>
+            Share your name and location. Receive the name and location of last person who sent love.
+          </div>
+          <div>
+            And eventually the name and location of the person after you.
+          </div>
+          <div>PANE 3</div>
+        </ReactSwipe>
       </div>
     </div>
   );
