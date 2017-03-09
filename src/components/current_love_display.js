1deeca3ae08db9627a0b7dbfdf84cd4dba26a60a
Shane <shanebeehler@me.com>
2017-03-08 22:01:54 -0500

    update profile styles

--- src/components/current_love_display.js
+++ src/components/current_love_display.js
@@ -7,44 +7,64 @@ const CurrentLoveDisplay = ({profile}) => {
     if (current_love[2] != null) {
       return (
         <div className="profile">
-          <div>
-            <p className="fancy">inhale love from</p>
+          <div className="card">
+            {/* <p className="fancy">inhale love from</p> */}
             <p>{current_love[0].name}</p>
-            <div>
-              {Math.round(current_love[0].distance_from_last_city)} KM
-            </div>
-            <p className="fancy in">away in</p>
+            <p className="fancy in">in</p>
             <p>{current_love[0].city}, {current_love[0].country}</p>
           </div>
 
+          <div>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
+            <div className="distance">
+              {Math.round(current_love[1].distance_from_last_city)}<span> KM</span>
+            </div>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
+          </div>
 
+          <div>to YOU in {current_love[1].city}</div>
 
           <div>
-            <p className="fancy">exhale love to</p>
-            <p>{current_love[2].name}</p>
-            <p className="fancy">in</p>
-            <p>{current_love[2].city}, {current_love[2].country}</p>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
+            <div className="distance">
+              {Math.round(current_love[2].distance_from_last_city)}<span> KM</span>
+            </div>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
           </div>
+            <div className="card">
+              {/* <p className="fancy">inhale love from</p> */}
+              <p>{current_love[2].name}</p>
+              <p className="fancy in">in</p>
+              <p>{current_love[2].city}, {current_love[2].country}</p>
+            </div>
         </div>
       )
     }
     else {
       return (
         <div className="profile">
-          <div>
-            <p className="fancy">inhale love from</p>
+          <div className="card">
+            {/* <p className="fancy">inhale love from</p> */}
             <p>{current_love[0].name}</p>
-            <div>
-              {Math.round(current_love[0].distance_from_last_city)} KM
-            </div>
-            <p className="fancy in">away in</p>
+            <p className="fancy in">in</p>
             <p>{current_love[0].city}, {current_love[0].country}</p>
           </div>
 
           <div>
-            <p className="fancy">exhale love to</p>
-            <p>the rest of the world</p>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
+            <div className="distance">
+              {Math.round(current_love[1].distance_from_last_city)}<span> KM</span>
+            </div>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
+            <p><i className="fa fa-heart fancy" aria-hidden="true"></i></p>
           </div>
+
+          <div>to YOU in {current_love[1].city}</div>
         </div>
       )
     }
