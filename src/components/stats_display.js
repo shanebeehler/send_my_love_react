65464d9debc41b65a310c230dc9b08f6a163c1d3
Adam Tse <mr.adam.tse@gmail.com>
2017-03-23 23:20:19 -0400

    - Added react count-up - count added to stats, over duration 3

--- src/components/stats_display.js
+++ src/components/stats_display.js
@@ -1,11 +1,12 @@
 import React   from 'react';
+import CountUp from 'react-countup';
 
 const StatsDisplay = ({title, stats, count}) => {
   if (stats != null) {
     return (
       <div>
         <p>{title}</p>
-        <p>{stats[count].toString().replace(/(\d)(?=(\d{3})+$)/g, '$1 ')}</p>
+        <p>  <CountUp start={0} end={stats[count]} duration={3} /></p>
       </div>
     )
   }
